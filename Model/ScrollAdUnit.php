8c04faa7c850e5a6a89fbbf288af8a8af47fcefa
Behçet Mutlu <behcet.mutlu@enuygun.com>
2016-12-16 18:23:20 +0200

    Scroll add unit updated to handle timeout, wait-timeout, close-button, initialize options

--- Model/ScrollAdUnit.php
+++ Model/ScrollAdUnit.php
@@ -21,12 +21,17 @@ class ScrollAdUnit extends AdUnit
         $class  = $this->getClass($settings->getDivClass());
         $style  = $this->getStyles();
         $attrs  = $this->getAttrsAsString();
+        $attr_array  = $this->getAttrs();
+        $closed = isset($attr_array['data-closed']) ? $attr_array['data-closed'] : 'false';
+        $scrolled = isset($attr_array['data-scrolled']) ? $attr_array['data-scrolled'] : 'false';
+        $initTimer = isset($attr_array['data-init-timer']) ? $attr_array['data-init-timer'] : 2000;
+        $closeBtn = isset($attr_array['data-close-btn']) ? $attr_array['data-close-btn'] : 'scrolldown_ad_close';
 
         return <<< RETURN
 <div class="{$class}"{$attrs}>
 <div id="{$this->divId}">
-<a href="javascript:void(0)" class="scrolldown_ad_close">x</a>
-<script type="text/javascript" defer>
+<a href="javascript:void(0)" class="{$closeBtn}">x</a>
+<script type="text/javascript">
 googletag.cmd.push(function() { googletag.display('{$this->divId}'); });
 googletag.cmd.push(function() {
     $(function() {
@@ -34,25 +39,65 @@ googletag.cmd.push(function() {
             clearTimeout(slideTimer);
             $('div.{$class}').each(function() {
                 var ad = $(this);
-                var closed = false;
-                var scrolled = false;
-                var showAd = function(force) { if(! closed || force) { ad.toggle('slow'); scrolled = true; closed = false; } }
-                            var hideAd = function() { ad.fadeOut(); closed = true; }
+                var closed = {$closed};
+                var scrolled = {$scrolled};
+                var disabled = false;
+                var showAd = function(force) { if(! closed || force) { ad.toggle('fast'); scrolled = true; closed = false; } }
+                    var hideAd = function(forced) { if(!forced && disabled) return; ad.fadeOut(); closed = true; }
 
-                            if(ad.find('div').css('display') != 'none') {
+                    if(ad.find('div').css('display') != 'none') {
+                        showAd();
+                        $(window).scroll(function () {
+                            if (!scrolled && !closed && $(this).scrollTop() > 40) {
                                 showAd();
-                                $(window).scroll(function () {
-                                    if (!scrolled && !closed && $(this).scrollTop() > 40) {
-                                        showAd();
-                                    }
-                                });
-                                ad.find('.scrolldown_ad_close').click(function(e) {
-                                    e.preventDefault();
-                                    hideAd();
-                                });
                             }
                         });
-                    }, 2000);
+                        ad.find('.{$closeBtn}').click(function(e) {
+                            e.preventDefault();
+                            hideAd();
+                        });
+                    }
+                    
+                    var timeout = ad.data('timeout');
+                    var timeout_wait = ad.data('wait-timeout');
+                   
+                    if(timeout) {
+                        setTimeout(function(){
+                            // Hide function
+                            hideAd(true);
+                        }, timeout);
+                    }
+                    
+                    if(timeout_wait) {
+                        var count=timeout_wait/1000;
+
+                        var counter=setInterval(timer, 1000);
+                        $('.{$class} .{$closeBtn}')
+                                .html('' + count +'')
+                                .css('display','inline');
+                    
+                    
+                        function timer()
+                        {
+                            $('.{$class} .{$closeBtn}').html(--count);
+                            if (count <= 0)
+                            {
+                                clearInterval(counter);
+                                return;
+                            }
+                    
+                        }
+                    
+                       // Disable Close Button
+                        disabled = true;
+                        setTimeout(function(){
+                            // Enable Close Button
+                            $('.{$class} .{$closeBtn}').html('x');
+                            disabled = false;
+                        }, timeout_wait);
+                    }
+                });
+            }, {$initTimer});
     });
 });
 </script>
